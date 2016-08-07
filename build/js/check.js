69c1fb69c34d3cdf30059dff27f223c0610929fc
Sveta <smaltseva00@gmail.com>
2016-08-07 13:40:53 +0300

    Исправленная функция

--- build/js/check.js
+++ build/js/check.js
@@ -1,25 +1,33 @@
 // Функция получает значения, проверяет их и возвращает сообщения
 
 function getMessage (a, b) {
-    if (typeof (a) === 'boolean' {
+    var numberOfSteps;
+
+    function summOfArray (arr) {
+      var sum = 0;
+      for (var i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+      }
+      return sum;
+    }
+    if (typeof (a) === 'boolean') {
+      var a = true;
       if (a) {
         return ('Я попал в b');
-    } // если а true
+    } // если а true 
       else  {
         return ('Я никуда не попал');
     } // если a false
   }
     if (typeof (a) === 'number') {
-      return ('Я прыгнул на ' + a * 100 + ' сантиметров';)
+      return ('Я прыгнул на ' + a * 100 + ' сантиметров')
   }
-    if (Array.isArray(a) {
--     var numberOfSteps = summArrays(a);
--     return 'Я прошёл '+ numberOfSteps +' шагов';
--  }
+    if (Array.isArray(a)) {
+      numberOfSteps = summOfArray(a);
+      return 'Я прошёл '+ numberOfSteps +' шагов';
+}
     if (Array.isArray(a) && Array.isArray(b)) {
-+     var distancePath = calculatePath(a, b);
-+     return 'Я прошёл ' + distancePath + ' метров';
+      var distancePath = calculatePath(a, b);
+      return 'Я прошёл ' + distancePath + ' метров';
   }
 };
-
-// getMessage (a, b); - надо ли вызывать функцию или она вызывается в другом js файле?
