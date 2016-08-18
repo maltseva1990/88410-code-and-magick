bc046bfcb440f36d2e9fafb6b56f5b6c4eb33099
Sveta <smaltseva00@gmail.com>
2016-08-18 12:11:05 +0300

    Добавлена валидация формы

--- src/form.js
+++ src/form.js
@@ -1,9 +1,21 @@
 'use strict';
 
+
 window.form = (function() {
   var formContainer = document.querySelector('.overlay-container');
   var formCloseButton = document.querySelector('.review-form-close');
-
+  /*Находим элементы, с которыми будем работать и сохраняем их в переменные*/
+  var formReview = formContainer.querySelector('.review-form');
+  var reviewMark = formReview.elements['review-mark'];
+  var reviewName = formReview.elements['review-name'];
+  var reviewText = formReview.elements['review-text'];
+  var reviewSubmitButton = formReview.querySelector('.review-submit');
+  var reviewFields = formReview.querySelector('.review-fields');
+  var reviewFieldsName = formReview.querySelector('.review-fields-name');
+  var reviewFieldsText = formReview.querySelector('.review-fields-text');
+  /*Добавляем обработчик событий*/
+  reviewName.addEventListener('input', validateForm);
+  reviewText.addEventListener('input', validateForm);
   var form = {
     onClose: null,
 
@@ -23,12 +35,30 @@ window.form = (function() {
       }
     }
   };
+/*Валидация формы*/
+  function validateForm() {
+  /*Проверяем поле имя*/
+    reviewName.required = true;
+    var NameIsValid = Boolean(reviewName.value);
+    switchOrToggle(reviewFieldsName, !NameIsValid);
 
+  /*Проверяем, если менее трех звезд*/
+    reviewText.required = reviewMark.value < 3;
+    var TextIsValid = !reviewText.required || reviewText.value;
+    switchOrToggle(reviewFieldsText, !TextIsValid);
 
+  /*Добавляем кнопку и ссылки*/
+    var isFieldsValid = NameIsValid && TextIsValid;
+    reviewSubmitButton.disabled = !isFieldsValid;
+    switchOrToggle(reviewFields, !isFieldsValid);
+  }
+  /*Функция, чтобы показать/скрыть элемент*/
+  function switchOrToggle(elem, show) {
+    elem.classList.toggle('invisible', !show);
+  }
   formCloseButton.onclick = function(evt) {
     evt.preventDefault();
     form.close();
   };
-
   return form;
 })();
--- src/main.js
+++ src/main.js
@@ -2,4 +2,26 @@
 
 require('./form.js');
 require('./game.js');
-require('./game-stat.min.js');
+
+
+(function() {
+  var game = new window.Game(document.querySelector('.demo'));
+  game.initializeLevelAndStart();
+  game.setGameStatus(window.Game.Verdict.INTRO);
+
+  var formOpenButton = document.querySelector('.reviews-controls-new');
+
+  /** @param {MouseEvent} evt */
+  formOpenButton.onclick = function(evt) {
+    evt.preventDefault();
+
+    window.form.open(function() {
+      game.setGameStatus(window.Game.Verdict.PAUSE);
+      game.setDeactivated(true);
+    });
+  };
+
+  window.form.onClose = function() {
+    game.setDeactivated(false);
+  };
+})();
